09c6d0016ee0d3f897c65d54fbc2514ebe4bbb6d
Sam Holton <sholton311@gmail.com>
2016-11-05 23:37:04 -0500

    first commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,23 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+
+// TODO: See which ones of the following are necessary and remove otherwise
+//= require jquery.mobile.custom
+//= require jquery.cookie
+//- require jquery.fancybox
+//= require sw_megamenu
+//= require owl.carousel.min
+//= require_tree .
