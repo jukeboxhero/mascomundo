d994fb51382da6b46d73d0187557e86e8af2a5e2
Sam Holton <sholton311@gmail.com>
2016-12-04 22:09:37 -0500

    troubleshooting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,7 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.assets.initialize_on_precompile = false
+  config.consider_all_requests_local = true
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
