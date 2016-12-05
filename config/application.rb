84c957c2ce59a7d32b167c9e9a5700c0dd362194
Sam Holton <sholton311@gmail.com>
2016-12-04 22:25:43 -0500

    serve static assets

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module MascoMundo
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.assets.initialize_on_precompile = false
+    config.serve_static_assets = true
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
